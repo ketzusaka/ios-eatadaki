9856c4238d99ae21c875f893e519f3fc96fcf643
James Richard <ketzu@me.com>
2025-12-10 16:30:49 -0800

    Themify our loading view

--- EatadakiUI/Sources/Utilities/LoadingView.swift
+++ EatadakiUI/Sources/Utilities/LoadingView.swift
@@ -1,15 +1,35 @@
 import SwiftUI
 
 public struct LoadingView: View {
-    public init() {}
+    @Environment(ThemeManager.self) private var themeManager
+    @Environment(\.colorScheme) private var colorScheme
+
+    let text: String
+
+    public init(text: String = "Loading...") {
+        self.text = text
+    }
 
     public var body: some View {
+        let theme = themeManager.tokens(for: colorScheme)
+
         VStack {
             ProgressView()
-            Text("Loading...")
-                .font(.caption)
-                .foregroundColor(.secondary)
+            Text(text)
+                .captionTextStyling(using: theme)
                 .padding(.top, 8)
         }
     }
 }
+
+#if DEBUG
+#Preview {
+    LoadingView()
+        .environment(ThemeManager())
+}
+
+#Preview("Custom Text") {
+    LoadingView(text: "Fetching data...")
+        .environment(ThemeManager())
+}
+#endif
