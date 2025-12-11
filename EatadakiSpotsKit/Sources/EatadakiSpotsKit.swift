12102888c765a32da76bdd5c4fe3ad27e2ef67f4
James Richard <ketzu@me.com>
2025-12-10 16:24:43 -0800

    Remove no longer needed placeholder file

--- EatadakiSpotsKit/Sources/EatadakiSpotsKit.swift
+++
@@ -1,9 +0,0 @@
-import EatadakiData
-import EatadakiKit
-import EatadakiLocationKit
-import Foundation
-
-/// EatadakiSpotsKit provides spots-related functionality for the Eatadaki app.
-public struct EatadakiSpotsKit {
-    public init() {}
-}
